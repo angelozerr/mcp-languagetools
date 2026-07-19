8d268e3efcf0d4c2e5b86817e134b6139c050585
azerr <azerr@redhat.com>
2026-07-19 11:16:23 +0200

    Add "IBM Corporation" header
    Signed-off-by: azerr <azerr@redhat.com>

--- core/src/main/java/com/ibm/mcp/languagetools/language/LanguageDocument.java
+++ core/src/main/java/com/ibm/mcp/languagetools/language/LanguageDocument.java
@@ -1,13 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2026 Red Hat, Inc.
- * Distributed under license by Red Hat, Inc. All rights reserved.
- * This program is made available under the terms of the
- * Eclipse Public License v2.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v20.html
+ * Copyright (c) 2026 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- * Red Hat, Inc. - initial API and implementation
- ******************************************************************************/
+ *     Angelo ZERR - initial API and implementation
+ *******************************************************************************/
 package com.ibm.mcp.languagetools.language;
 
 import java.net.URI;
--- core/src/main/java/com/ibm/mcp/languagetools/mcp/McpClientTracker.java
+++ core/src/main/java/com/ibm/mcp/languagetools/mcp/McpClientTracker.java
@@ -1,13 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2026 Red Hat Inc. and others.
- * All rights reserved. This program and the accompanying materials
+ * Copyright (c) 2026 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v20.html
+ * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *     Red Hat Inc. - initial API and implementation
+ *     Angelo ZERR - initial API and implementation
  *******************************************************************************/
 package com.ibm.mcp.languagetools.mcp;
 
